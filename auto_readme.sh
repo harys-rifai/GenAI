d9ae2e1e84fffd0c8da6ae277816405badd496f9
harys rifai <harysrifai@gmail.com>
2026-05-03 11:34:01 +0700

    Auto-update README on 2026-05-03 11:34:01

--- auto_readme.sh
+++ auto_readme.sh
@@ -11,21 +11,9 @@ echo "" >> README.md
 echo "Generated on: $(date '+%Y-%m-%d %H:%M:%S')" >> README.md
 echo "" >> README.md
 
-# Tambahkan tree struktur repo
-echo "## 📂 Project Structure" >> README.md
-echo '```' >> README.md
-tree -a -I '.git|venv|__pycache__' >> README.md
-echo '```' >> README.md
-echo "" >> README.md
-
-# Tambahkan daftar file
-echo "## 📄 Files in Repo" >> README.md
-git ls-files >> README.md
-echo "" >> README.md
-
 # Tambahkan log commit terakhir
 echo "## 📝 Recent Commits" >> README.md
-git log -n 5 --pretty=format:"- %h %s (%cr)" >> README.md
+git log -n 10 --pretty=format:"- %h %s (%cr)" >> README.md
 echo "" >> README.md
 
 # Stage semua perubahan
